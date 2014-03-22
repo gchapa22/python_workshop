131dfc594b3480c489ce645885b2b811792b9019
gchapa22 <gerardo.chapa.q@gmail.com>
2014-03-21 23:18:01 -0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,50 @@
+# Variables
+
+a = 1
+
+a, b = 1, 2
+
+b = a = 3
+
+# TIPOS
+
+# NUMEROS
+# ---------------------
+# Enteros (int)
+a = 1
+b = 2
+
+# Long
+d = 51924361L
+e = 0xDEFABCECBDAECBFBAEl
+
+# Float
+f = 3.14159
+g = 32.3+1e18
+
+# Complex
+h = 3.2j
+i = 45.1j
+
+# STRINGS
+# ----------------------
+j = 'Spam'
+k = "'Tis but a scratch."
+l = 'We are now the Knights who say... "Ekki-Ekki-Ekki-Ekki-PTANG. Zoom-Boing. Znourrwringmm.'
+
+# LISTS
+# ----------------------
+m = [1,2,3,4,5,6]
+n = [1,"spam",k,[1,2,3]]
+o = [[1,2,3],
+     [4,5,6],
+     [7,8,9]]
+
+# TUPLES
+# ----------------------
+p = ('Guido','van Rossum',1956,'Dutch')
+q = ('Spam',3.14159)
+
+# DICTIONARY
+# ----------------------
+r = {'pi':3.14159, 'euler':2.71828, 'tau':6.28318,'phi':1.61803}
