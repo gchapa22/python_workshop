49783748295dd4be51d95caadedb146ff2f71ea1
gchapa22 <gerardo.chapa.q@gmail.com>
2014-03-29 17:11:23 -0600

    Create condicionales.py

---
+++ condicionales.py
@@ -0,0 +1,19 @@
+#CONDITIONAL STATEMENTS
+x = False
+# IF
+if x:
+	print x
+
+# IF ELSE
+if x:
+	print x
+else:
+	print "No",x
+
+# IF ELIF ELSE
+if x:
+	print x
+elif not x:
+	print "No", x
+else:
+	print "None"
