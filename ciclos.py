ff44712b793f8965effab918f61c9ebc3b14d95f
tomascaslo <a01033534@itesm.mx>
2014-03-29 19:25:05 -0600

    Create ciclos.py

---
+++ ciclos.py
@@ -0,0 +1,19 @@
+#Ciclos
+#For
+
+#While
+cuenta = 0
+while cuenta < 10:
+	print "while: Hola amigos", cuenta 
+	cuenta += 1
+print "\n"
+
+#For
+for i in range(10):
+	print "for (range): Hola amigos", i
+print "\n"
+
+lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in lista:
+	print "for (list): Hola amigos", i
+print "\n"
