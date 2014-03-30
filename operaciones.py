beb1d3daa0d31c75b804431fe6a355b5a6b152ac
gchapa22 <gerardo.chapa.q@gmail.com>
2014-03-29 18:25:54 -0600

    Create operaciones.py

---
+++ operaciones.py
@@ -0,0 +1,72 @@
+# OPERACIONES
+
+# Operaciones Aritmeticas 
+# --------------------------
+# Suma
+1 + 1
+# Resta
+100 - 19
+# Multiplicacion
+2 * 3
+# Division
+100/2
+# Division Floor
+100//3
+# Potencia
+2**2
+# Modulo
+100%30
+# Concatenacion
+"A"+"B"
+
+# Operaciones de Comparacion
+# --------------------------
+1 == 1
+1 != 2
+1 <> 2
+2 > 1
+1 < 2
+1 >= 1
+1 <= 2
+
+# Operaciones de Asignacion
+# --------------------------
+x=1
+x+=1
+x-=1
+x*=1
+x/=1
+x%=1
+x**=1
+x//=1
+
+# Operaciones Logicas
+# --------------------------
+False and True
+True or False
+not False
+
+# Operaciones de Membership
+# --------------------------
+1 in [1,2,3,4]
+"Spam" not in [1,2,3,4]
+
+# Operaciones de Identidad
+# --------------------------
+1 is 1
+"uno" is not 1
+
+# Orden de Precedencia
+# **	Exponentiation (raise to the power)
+# ~ + -	Ccomplement, unary plus and minus (method names for the last two are +@ and -@)
+# * / % //	Multiply, divide, modulo and floor division
+# + -	Addition and subtraction
+# >> <<	Right and left bitwise shift
+# &	Bitwise 'AND'
+# ^ |	Bitwise exclusive `OR' and regular `OR'
+# <= < > >=	Comparison operators
+# <> == !=	Equality operators
+# = %= /= //= -= += *= **=	Assignment operators
+# is is not	Identity operators
+# in not in	Membership operators
+# not or and	Logical operators
