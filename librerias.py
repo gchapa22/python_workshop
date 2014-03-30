9b00ca054eeb52c2eed82811ea28c108847e538b
tomascaslo <a01033534@itesm.mx>
2014-03-29 19:25:42 -0600

    Create librerias.py

---
+++ librerias.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#Librerías
+
+import math
+
+print "\n"
+print "Módulo Math"
+print "Pi: ", math.pi
+print "Potencia (2^10): ", int(math.pow(2, 10))
+print "\n"
+
+import librerias.math_funcs as Mate
+
+print "Módulo personalizado"
+print "Pi: ", Mate.pi
+print "Potencia (2^10): ", Mate.pow(2, 10)
+print "\n"
